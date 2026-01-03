6f1085e04ac9158a4d51b1404c46aad47570976d
Deepak Singh <deepakksingh1202@gmail.com>
2026-01-04 00:36:32 +0530

    remove comment in authLayout.

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,27 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
-// export default function Protected({ children, authentication = true }) {
-
-//   const navigate = useNavigate();
-//   const [loading, setLoading] = useState(true);
-
-//   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-
-//   useEffect(() => {
-//     if (authentication && !isAuthenticated) {
-//       navigate("/login");
-//     }
-//     if (!authentication && isAuthenticated) {
-//       navigate("/");
-//     }
-
-//     setLoading(false);
-//   }, [authentication, isAuthenticated, navigate]);
-
-//   return loading ? <h1>Loading...</h1> : <>{children}</>;
-// }
 
 export default function Protected({ children, authentication = true }) {
   const navigate = useNavigate();
