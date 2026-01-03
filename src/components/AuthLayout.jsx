a373dc70e41d9e1ed6fcafe5bb675c9b5d8eda8b
Deepak Singh <deepakksingh1202@gmail.com>
2026-01-04 00:30:46 +0530

    update AuthLayout

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -2,23 +2,43 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
+// export default function Protected({ children, authentication = true }) {
+
+//   const navigate = useNavigate();
+//   const [loading, setLoading] = useState(true);
+
+//   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+//   useEffect(() => {
+//     if (authentication && !isAuthenticated) {
+//       navigate("/login");
+//     }
+//     if (!authentication && isAuthenticated) {
+//       navigate("/");
+//     }
+
+//     setLoading(false);
+//   }, [authentication, isAuthenticated, navigate]);
+
+//   return loading ? <h1>Loading...</h1> : <>{children}</>;
+// }
+
 export default function Protected({ children, authentication = true }) {
-
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const { isAuthenticated, authChecked } = useSelector((state) => state.auth);
 
   useEffect(() => {
+    if (!authChecked) return;
+
     if (authentication && !isAuthenticated) {
       navigate("/login");
     }
     if (!authentication && isAuthenticated) {
       navigate("/");
     }
+  }, [authentication, isAuthenticated, authChecked, navigate]);
 
-    setLoading(false);
-  }, [authentication, isAuthenticated, navigate]);
+  if (!authChecked) return <h1>Loading...</h1>;
 
-  return loading ? <h1>Loading...</h1> : <>{children}</>;
+  return <>{children}</>;
 }
